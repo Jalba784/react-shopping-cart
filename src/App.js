12101cfd4fa6c029bfcb789b9c2144e9d747ac7b
josepaniagua <jpanjr@gmail.com>
2019-12-18 18:12:53 -0800

    created productcontext and wrapped all of app.js with it

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+
+// Contexts
+import { ProductContext } from "./contexts/ProductContext";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-		setCart(cart => [...cart, item])
-	};
+  const addItem = item => {
+    // add the given item to the cart
+    setCart(cart => [...cart, item]); // Each time i go into the cart and add a new item.
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  return (
+    <ProductContext.Provider>
+      <div className="App">
+        <Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+        {/* Routes */}
+        <Route
+          exact
+          path="/"
+          render={() => <Products products={products} addItem={addItem} />}
+        />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+      </div>
+    </ProductContext.Provider>
+  );
 }
 
 export default App;
